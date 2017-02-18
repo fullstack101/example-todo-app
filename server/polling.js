00a0f5dfa9becdab64e8f597f0d9e2c73e950238
Mihail Mikov <m.mikoff@gmail.com>
2017-02-18 23:55:50 +0200

    removed polling logging, fixed npm scripts

--- server/polling.js
+++ server/polling.js
@@ -4,10 +4,8 @@ const polling = new Map();
 
 module.exports = {
     subscribe(id, callback) {
-        console.log('added sub', id);
         polling.set(id, callback);
         return () => {
-          console.log('deleted sub', id); 
           polling.delete(id);
         };
     },
@@ -15,7 +13,6 @@ module.exports = {
       polling.forEach((cb, id) => {
           cb(message);
           polling.delete(id);
-          console.log('called and deleted sub', id); 
       });
     }
 };
